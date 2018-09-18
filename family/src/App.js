a89b41fa60671c91ec47d81193a6526723322138
Daniel <olivas1406@msn.com>
2018-09-18 08:55:54 -0700

    Added navbar

--- family/src/App.js
+++ family/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import "./style.css";
+import navbar from "./Components/navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <navbar>NavbarTest1 NavbarTest2 NavbarTest3</navbar>
       </div>
     );
   }
---
+++ family/src/Components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../style.css";
+
+const Navbar = props => (
+    <div className="navbar">{props.children}
+    </div>
+);
+
+export default Navbar;
